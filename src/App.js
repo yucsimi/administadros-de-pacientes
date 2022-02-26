2f526dd4afbd198c34c442bee68f2480a4a7849a
yucsimi <yucsimigliced14@gmail.com>
2022-02-26 08:33:39 -0800

    warning

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
     } else {
       localStorage.setItem("citas", JSON.stringify([]));
     }
-  }, [citas]);
+  }, [citas, citasIniciales]);
 
   //funcion que tome las citas actuales y agregue las nuevas
   const crearCita = (cita) => {
